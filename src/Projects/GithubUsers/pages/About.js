7797369203fa22c8ff5274d7f8ac299790d9d573
Daniel1991T <dantifui.91@gmail.com>
2022-05-12 20:06:10 +0300

    refactor the github search project

--- src/Projects/GithubUsers/pages/About.js
+++ src/Projects/GithubUsers/pages/About.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const About = () => {
     return (
-        <section className='text-center p-4 bg-gray-400 h-[90%]'>
-            <h1 className='text-mainWhite'>about</h1>
+        <section className='flex flex-col items-center p-4 h-[90%] space-y-6'>
+            <h1 className='text-mainWhite text-6xl font-normal mb-4'>Github Finder</h1>
+            <p className='mb-4 text-2xl font-light max-w-6xl'>This is a React.js project, where I used and learn
+                <strong> useContext with useReducer, fetch data from API with axios where I create my own axios with
+                    interceptors to add to request headers authorization prop my github token!</strong></p>
+            <p>To see the github repositorie visit next
+                <a className='link text-cyan-600 ml-2' href="https://github.com/Daniel1991T/rect-mini-projects-with-tailwindcss/tree/main/src/Projects/GithubUsers">
+                    link</a></p>
         </section>
     )
 }
